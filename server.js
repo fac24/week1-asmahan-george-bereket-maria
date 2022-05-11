6ea198a9629541311f92d06d8ebab23cf60c19d7
= <maria.louisa.castro@gmail.com>
2022-05-11 11:47:10 +0100

    Create and link css file

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require("express");
+
 const server = express();
 
-server.get('/', (request, response) => {
-   response.send('hello')
-})
+const staticHandler = express.static("public");
+server.use(staticHandler);
+
+server.get("/", (request, response) => {
+  response.send("hello");
+});
 
 const PORT = 3000;
-server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT} `))
+server.listen(PORT, () =>
+  console.log(`Listening on http://localhost:${PORT} `)
+);
