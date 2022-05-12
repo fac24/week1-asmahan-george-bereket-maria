5f0d54275871ee94a9231f6a78510d7a0dfa05de
= <maria.louisa.castro@gmail.com>
2022-05-12 16:00:56 +0100

    Re-align flex button

--- routes/home.js
+++ routes/home.js
@@ -2,13 +2,15 @@ const blogPosts = require("../blogPosts.js");
 const colours = require("../public/colours.js");
 
 function get(request, response) {
-  const crocsForm = `<div class="form-div"><form action="/crocspace-comments" method="POST">
+  const crocsForm = `<div class="form-div">
+  <form action="/crocspace-comments" method="POST">
 <label for="username">Username:</label>
 <input type="text" id="username" placeholder="username" name="name" class="usernamebox"><br>
 <label for="usermessage">Message:</label>
 <input type="text" id="usermessage" placeholder="enter message" message="message" class="messagebox"> <br/>    
-<button class="submit" type="submit" required>Submit</button>
-</form></div>`;
+<button class="submit" type="submit" required aria-label"submit post">Submit</button>
+</form>
+</div>`;
 
   let posts = "";
 
@@ -28,11 +30,13 @@ function get(request, response) {
             <h3>${post.user}</h3>
             <p>${post.message}</p>
           </div>
+          <div class="button-div">
           <form action="/delete-post" method="POST">
             <button class="delete" name="name" value="${postKey}" aria-label="Delete ${postKey}">
               Delete
             </button>
           </form>
+          </div>
         </div>
         `;
   }
