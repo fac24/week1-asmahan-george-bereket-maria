e42b49430e5770c13e4071b2794194103aa58d09
glombort <george.lambert99@gmail.com>
2022-05-12 14:43:49 +0100

    Slight Refactor
    Merged all our work together:)

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const home = require("./routes/home.js");
+const deletePost = require("./routes/deletePost")
 
 const server = express();
 
@@ -8,29 +9,12 @@ server.use(staticHandler);
 
 server.get('/', home.get);
 
-server.get("/", (request, response) => {
-  response.send(`
-  <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Crocspace</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@800&family=Roboto:wght@300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="style.css" />
-  </head>
-  <body>
-  <h1>hel<span class="green">lo</span></h1>
-  </body>
-</html>
+const bodyParser = express.urlencoded({extended: false})
 
-  `);
-});
+server.post("/delete-post", bodyParser, deletePost.post)
 
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () =>
   console.log(`Listening on http://localhost:${PORT} `)
 );
