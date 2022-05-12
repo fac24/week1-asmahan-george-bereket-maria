ffe06c0891422ea900bb1b036edc6267f2cab9fa
= <maria.louisa.castro@gmail.com>
2022-05-12 15:52:20 +0100

    Add lang tag
    Realtes #15

--- routes/home.js
+++ routes/home.js
@@ -38,7 +38,7 @@ function get(request, response) {
   }
   const html = `
     <!doctype html>
-    <html>
+    <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
