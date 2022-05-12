638772f0bef6c4ad8a72424ddd463a1d8d444606
= <maria.louisa.castro@gmail.com>
2022-05-12 10:47:40 +0100

    Initial heroku

--- blogPosts.js
+++ blogPosts.js
@@ -1,9 +1,12 @@
 let posts = {
-    0: {user: 'George', message: 'Alligator &gt Crocodile'},
-    1: {user: 'Asmahan', message: ''},
-    2: {user: 'Maria', message: ''},
-    3: {user: 'Bereket', message: ''},
-    4: {user: 'Elon', message: 'Damn this is better than twitter. Can I buy? &#128176;'},
+  0: { user: "George", message: "Alligator &gt Crocodile" },
+  1: { user: "Asmahan", message: "" },
+  2: { user: "Maria", message: "" },
+  3: { user: "Bereket", message: "" },
+  4: {
+    user: "Elon",
+    message: "Damn this is better than twitter. Can I buy? &#128176;",
+  },
 };
 
 module.exports = posts;
