15558dfee0f7368c7927112abbb5888b2216b544
Asmahan <asmahanhmohamed@gmail.com>
2022-05-12 13:43:57 +0100

    Merge attempt

--- routes/home.js
+++ routes/home.js
@@ -5,48 +5,89 @@ const server = express();
 const staticHandler = express.static("public");
 server.use(staticHandler);
 
-function get(request, response) {
-  let posts = "";
-  for (const postKey in blogPosts) {
-    post = blogPosts[postKey]
-    posts += `
-        <div class="blog-post">
-          <div>
-            <h3>${post.user}</h3>
-            <p>${post.message}</p>
-          </div>
-          <form action="/delete-post" method="POST">
-            <button class="delete" name="name" value="${postKey}" aria-label="Delete ${postKey}">
-              Delete
-            </button>
-          </form>
-        </div>
-        `;
-  }
-  const html = `
-    <!doctype html>
+//array of messages
+let messagesArray = [
+  {username: "", message: ""}
+]
+
+let messagesList = "";
+
+let message = "";
+
+//user input
+const crocsForm = `<form action="/crocspace-comments" method="POST">
+<label for="username">User:</label>
+<input type="text" id="username" placeholder="username" name="name"><br>
+<label for="usermessage">Message:</label>
+<input type="text" id="usermessage" placeholder="enter message" message="message"><br>       
+<input type="Submit" required value="Submit"></button>
+</form>`;
+
+server.get("/", (request, response) => {
+messageList = "";
+  
+  messagesArray.forEach((post) => {
+    message  = `<li>
+    <p>User: ${post["username"]} </p>
+    <p>Message ${post["message"]}</p>
+
+    <form action="/delete-posts" method="POST">
+    <button name="${post["username"]}" value="${post["message"]} <form>
+    </li>`;
+
+messageList += message;
+});
+
+const html = `<!doctype html>
     <html>
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Crocspace</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@800&family=Roboto:wght@300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="style.css" />
-  </head>
+      <head>
+        <meta charset="utf-8">
+        <title>Crocspace</title>
+      </head>
       <body>
+      <h1>Leave a message!</h1>
+      ${crocsForm}
         <div>
             <header>
-                <h2>Pos<span class="green">ts</span></h2>
+                <h2>Posts</h2>
             </header>
             ${posts}
         </div>
       </body>
-    </html>
-    `;
-  response.send(html);
-}
+    </html>`;
 
-module.exports = { get };
+    response.send(html)
+});
+
+
+function get(request, response) {
+    let posts = '';
+    for (const post of Object.values(blogPosts)) {
+        posts += `
+        <div class="blog-post"><h3>${post.user}</h3> <p>${post.message}</p></div>
+        `
+    };
+    const html = `
+    <!doctype html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Crocspace</title>
+      </head>
+      <body>
+      <h1>Leave a message!</h1>
+      ${crocsForm}
+        <div>
+            <header>
+                <h2>Posts</h2>
+            </header>
+        </div>
+      </body>
+    </html>
+    `
+    response.send(html)
+};
+
+const bodyParser = express.urlencoded(); //allows us to read the HHTP post data
+
+module.exports = { get }
