7ebed082e8c7a879bf7f6723cd8b6714d23c909b
Maria Castro <82417131+mariaalouisaa@users.noreply.github.com>
2022-05-12 12:17:18 +0100

    Update blogPosts.js

--- blogPosts.js
+++ blogPosts.js
@@ -1,9 +1,9 @@
 let posts = {
-   0: { user: "George", message: "Alligator &gt Crocodile" },
-   1: { user: "Asmahan", message: "" },
-   2: { user: "Maria", message: "Crocs changed my life 👡👡👡👡👡👡👡" },
-   3: { user: "Bereket", message: "" },
-   4: { user: "Elon", message: "Damn this is better than twitter. Can I buy? &#128176;", },
+   0: {user: "George", message: "Alligator &gt Crocodile" },
+   1: {user: "Asmahan", message: "" },
+   2: {user: "Maria", message: "Crocs changed my life 👡👡👡👡👡👡👡" },
+   3: {user: "Bereket", message: "" },
+   4: {user: "Elon", message: "Damn this is better than twitter. Can I buy? &#128176;", },
 };
 
 module.exports = posts;
